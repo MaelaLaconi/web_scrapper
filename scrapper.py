d6532a176af32a450d394755177c6cc9f828fa1c
MaelaLaconi <44729941+MaelaLaconi@users.noreply.github.com>
2021-09-15 09:04:08 +0200

    get url from next pages

--- scrapper.py
+++ scrapper.py
@@ -8,34 +8,58 @@ Created on Tue Sep 14 17:27:22 2021
 import requests
 from bs4 import BeautifulSoup
 
-url = 'http://example.python-scraping.com/'
-
-response = requests.get(url)
-
-# ok if code equals 200
-if response.ok:
-    links = []
-    # print html code
-    # print(response.text)
-    
-    # html code ca be parsed
-    soup = BeautifulSoup(response.text, 'lxml')
-    
-    # get title from the web page
-    title = soup.find('title')
-    
-    tds = soup.findAll('td')
-    print(len(tds))
+links = []
+for i in range(26):
     
     
-    #[print(str(td) + '\n\n') for td in tds]
+    url = 'http://example.python-scraping.com/places/default/index/' + str(i)
     
-    for td in tds:
-        n = td.find('a')
-        link = n['href']
-        links.append(link)
-
-        print('http://example.python-scraping.com/' + link)
-else:
-    print("error with get url")
+    response = requests.get(url)
     
+    # ok if code equals 200
+    if response.ok == False:
+        print("error with get url")
+    
+        
+    else:
+        print("Page " + str(i))
+        # print html code
+        # print(response.text)
+        
+        # html code ca be parsed
+        soup = BeautifulSoup(response.text, 'lxml')
+        
+        # get title from the web page
+        title = soup.find('title')
+        
+        tds = soup.findAll('td')
+        #print(len(tds))
+        
+        
+        #[print(str(td) + '\n\n') for td in tds]
+        
+        for td in tds:
+            n = td.find('a')
+            link = n['href']
+            links.append('http://example.python-scraping.com' +  link)
+    
+print(len(links))
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
